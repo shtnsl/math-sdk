00ce680b0f188828632236e422a2c768a4e32c8e
kalo_popzlatev <kaloyan@backseatgaming.com>
2025-04-17 10:47:35 +0300

    Revert setup txt and remove newly added game

--- games/2_20_96/game_calculations.py
+++
@@ -1,7 +0,0 @@
-from src.executables.executables import Executables
-
-
-class GameCalculations(Executables):
-    """Helper functions specific to Ninja Rabbit."""
-    pass  # No extra methods needed
-
