422d71229561446247b2f959efe8b383fcfc8ffe
DecJA <DJArms@protonmail.com>
2025-04-18 17:10:29 +1000

    use alternate resolve method

--- src/config/paths.py
+++ src/config/paths.py
@@ -1,9 +1,9 @@
 """Define relevant paths to games, logic engine and optimization."""
 
 import os
+from pathlib import Path
 
-ABS_PATH = os.path.abspath(os.getcwd())
-PROJECT_PATH = os.path.abspath(os.getcwd())
+PROJECT_PATH = os.path.abspath(Path(__file__).resolve().parent.parent.parent)
 PATH_TO_ENGINE = PROJECT_PATH
 PATH_TO_GAMES = os.path.join(PROJECT_PATH, "games")
 OPTIMIZATION_PATH = os.path.join(PROJECT_PATH, "optimization_program")
