00d33aac558fa9ce34fb9eadae123d412a49e2b1
DecJA <DJArms@protonmail.com>
2025-05-13 12:17:05 +1000

    update git URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="carrotengine",
+    name="stakeEngine-mathSDK",
     version="0.0.0",
     python_requires=">=3.12",
     author="CarrotRGS",
